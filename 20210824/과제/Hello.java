421d66ce0ba71b9195f53557d4663dce705c9e3f
SangOk <sangok1993@naver.com>
2021-08-24 16:46:14 +0900

    up

---
+++ 20210824/과제/Hello.java
@@ -0,0 +1,9 @@
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		System.out.println(100);
+		System.out.println(12.34);
+		System.out.println("이상옥");
+		System.out.printf("%d %c %s %s %f", 30, 'A', "A", "string", 2.321);
+	}//main end
+} // class end
---
+++ workspace/day02_basic/src/ex01/basic/HelloJava.java
@@ -0,0 +1,19 @@
+package ex01.basic;
+
+public class HelloJava {
+	public static void main(String[] args) { //시작점(진입점)
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+		System.out.println(123);
+		System.out.printf("%d", 1);
+	} //main end
+}//class end
+
+/**
+ *  도큐먼트 주석 : 설명서를 만들어야하는데 
+ *  
+ */
+
+// package > class > method > field 
+// 모든 자료형은 메모리에 할당을 해야한다. static 을 하면 자동으로 메모리에 할당을 함. 
+// static 방식이 아니면 객체생성으로 메모리 할당이 가능하다.
