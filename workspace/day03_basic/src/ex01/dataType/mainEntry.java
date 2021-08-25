3f5136e88b7f81f687781808d73fc0647d31f114
SangOk <sangok1993@naver.com>
2021-08-25 10:24:40 +0900

    d

---
+++ workspace/day03_basic/src/ex01/dataType/mainEntry.java
@@ -0,0 +1,12 @@
+package ex01.dataType;
+
+public class mainEntry {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 9;
+		long y = 8;
+		
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
